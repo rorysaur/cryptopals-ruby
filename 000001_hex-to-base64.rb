986be06243bd06abf0411fd2218c2d66f6f21945
rorysaur <ou.rory@gmail.com>
2015-07-28 20:25:10 -0700

    generate hex dict instead of writing it manually

--- 000001_hex-to-base64.rb
+++ 000001_hex-to-base64.rb
@@ -33,24 +33,17 @@ end
 #
 # @return [Hash{String => Integer}]
 def hex_dict
-  {
-    "0" => 0,
-    "1" => 1,
-    "2" => 2,
-    "3" => 3,
-    "4" => 4,
-    "5" => 5,
-    "6" => 6,
-    "7" => 7,
-    "8" => 8,
-    "9" => 9,
-    "a" => 10,
-    "b" => 11,
-    "c" => 12,
-    "d" => 13,
-    "e" => 14,
-    "f" => 15
-  }
+  nums = ("0".."9").to_a
+  letters = ("a".."f").to_a
+  keys = nums + letters
+
+  dict = {}
+
+  keys.each_with_index do |key, idx|
+    dict[key] = idx
+  end
+
+  dict
 end
 
 # Accepts a string of hexadecimal digits and converts it to an array of bits.
